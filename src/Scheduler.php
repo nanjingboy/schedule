4913a1cf6063c0d972b7bff7178c3dea9799b67b
Tom.Huang <hzlhu.dargon@gmail.com>
2015-05-09 19:17:25 +0800

    Init primary files

---
+++ src/Scheduler.php
@@ -0,0 +1,33 @@
+<?php
+namespace Schedule;
+
+class Scheduler
+{
+    protected static $_instance = null;
+
+    protected $_jobs = array();
+
+    public static function instance()
+    {
+        if (static::$_instance === null) {
+            static::$_instance = new static();
+        }
+
+        return static::$_instance;
+    }
+
+    public function append()
+    {
+        $job = new Job();
+        array_push($this->_jobs, $job);
+        return $job;
+    }
+
+    final private function __construct()
+    {
+    }
+
+    final private function __clone()
+    {
+    }
+}
---
+++ test/src/JobTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace Test\Src;
+
+use Schedule\Job;
+use PHPUnit_Framework_TestCase;
+
+class JobTest extends PHPUnit_Framework_TestCase
+{
+}
